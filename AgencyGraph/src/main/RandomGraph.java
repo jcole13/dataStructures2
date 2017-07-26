e75d3007a2c60df6b27d11d6263b500b7b82d633
inisovivax <richard.gaoanhs@gmail.com>
2017-07-26 17:57:03 -0400

    clustering
    implemented a weak clustering tendency

--- AgencyGraph/src/main/RandomGraph.java
+++ AgencyGraph/src/main/RandomGraph.java
@@ -14,12 +14,14 @@ public class RandomGraph {
         for(int i=1;i<numnodes;i++)group[i]=new GraphNode(new People());
         for(GraphNode p: group)temp.addNode(p);
         Random rng=new Random();
-        int chance=(int)(Math.log10(numnodes*numnodes)*numnodes/26);
+        int chance=(int)(Math.log10(numnodes*numnodes)*numnodes/26*2);
         System.out.println(chance);
+        boolean cluster=false;
         for(int i=0;i<group.length-1;i++){
             int counter=0;
-            while(counter<1) {
-                for (int j = i + 1; j < group.length && counter < 4; j++) {
+            if(!cluster)
+             while(counter<1) {
+                for (int j = i + 1; j < group.length; j++) {
                     if ((rng.nextInt() % chance) == 2) {
                         System.out.println(group[i] + " is friends with " + group[j]);
                         temp.addEdge(group[i], group[j]);
@@ -27,6 +29,20 @@ public class RandomGraph {
                     }
                 }
             }
+            else
+                while(counter<1) {
+                    for (int j = i + 1; j < group.length && counter < 6; j++) {
+                        if ((rng.nextInt() % 3) > 1) {
+                            System.out.println(group[i] + " is friends with " + group[j]);
+                            temp.addEdge(group[i], group[j]);
+                            counter++;
+                        }
+                    }
+                    cluster=rng.nextInt()%6!=1;
+
+                }
+
+            cluster=cluster||rng.nextInt()%6==2;
         }
         return temp;
 
